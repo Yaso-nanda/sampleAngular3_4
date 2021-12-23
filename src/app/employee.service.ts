5f8298a42bc4dcf6ad02818427ba180343fc64eb
yasonanda <88535831+Yaso-nanda@users.noreply.github.com>
2021-12-23 18:28:15 +0530

    Update employee.service.ts

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://3.145.171.68:30165/api/v1/employees";
+  private baseURL = "http://3.145.171.68:30163/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
   
